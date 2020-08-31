152472089b1196ba941e53f8cb84eab110561dcd
Kiran Limbu <kiranlimbu@Kirans-MacBook-Pro.local>
2020-08-30 20:17:10 -0700

    Submission for  Module 8

--- ON/KiranLimbu/Module6/PE06.R
+++ ON/KiranLimbu/Module6/PE06.R
@@ -15,13 +15,23 @@
 set.seed(1234)
 group1 <- rnorm(100, mean = 85, sd = 5)
 
-set.seed(1234)
 group2 <- rnorm(100, mean = 85, sd = 5)
 
 num_set <- c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
-group2a <- append(group2, num_set)
 
+for(i in num_set) {
+    # add num_set (one vector at a time) to group2
+    group2a <- group2 + i
+    # the result of the test
+    result <- t.test (x = group1, y = group2a, var.equa=T)
 
-# the result of the test
-result <- t.test (x = group1, y = group2a, var.equa=T)
-print(result)
+    # check the result at the significance level of 5%
+    # if condition is met break from the loop
+    if (result$p.val < 0.05) {
+        print("The smallest constant in num_set that rejects the null hypothesis")
+        print(i)
+        print("p-value")
+        print(result$p.val)
+        break
+    }
+}
