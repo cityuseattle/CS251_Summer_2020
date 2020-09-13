d61883bed6fada739a81b2774c7f23628e46ccf2
deutscherjohan <johan.deutscher@gmail.com>
2020-09-12 20:23:25 -0700

    Submission for Module 10

---
+++ ON/JohanDeutscher/Module10/Dpois.r
@@ -0,0 +1,27 @@
+print(dpois(6,4.5))
+
+#simulating births in this hospital for ayear (n=365) using rpois()
+library(dplyr)
+library(ggplot2)
+
+set.seed(2)
+
+babies<-data.frame('data'=rpois(365,4.5))
+
+result<-babies%>%ggplot()+
+    geom_histogram(aes(x=data,
+                        y=stat(count/sum(count)),
+                        fill=data==6),
+                        color="black",
+                    binwidth=1)+
+    scale_x_continuous(breaks=0.10) +
+    labs(x='Number of babies born per period',
+        y='Proportion',
+        title='365 simulated births in a hospital with Pois(lambda=4.5)') +
+    theme_bw()
+
+print(result)
+
+#To see the simulate result is equal to the output of (dbois(6, 4.5))
+print(babies%>% dplyr::summarize(six_babies=sum(babies$data==6)/n()))
+
---
+++ ON/JohanDeutscher/Module10/Rpois.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(ggplot2)
+
+set.seed(2)
+
+poisson_data<-data.frame('data'=rpois(1000,10))
+
+result<-poisson_data%>%ggplot()+
+    geom_histogram(aes(x=data,
+                        y=stat(count/sum(count))),
+                        color="black",
+                    binwidth=1)+
+    geom_vline(xintercept=10,
+                size=1,
+                linetype='dashed',
+                color='red')+
+    theme_bw()+
+    labs(x='Number of success per period',
+        y='Proportion',
+        title='1,000 samples of Pois(Lambda=10)')
+
+print(result)
